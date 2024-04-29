fd322ee8dc21f73071f0f5a97d2e0dfc28d1f627
Tiberius202 <tiberius202@gmail.com>
2024-04-29 17:28:00 -0400

    changed pin to 14

--- hardware_functions.py
+++ hardware_functions.py
@@ -5,32 +5,32 @@ def setup():
     GPIO.setup(2, GPIO.OUT)
     GPIO.setup(3, GPIO.OUT)
     GPIO.setup(4, GPIO.OUT)
-    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(14, GPIO.OUT)
 
 def onlyOne(x : int):
     if (x == 0):
         GPIO.output(2, True);
         GPIO.output(3, False);
         GPIO.output(4, False);
-        GPIO.output(17, False);
+        GPIO.output(14, False);
     elif (x == 1):
         GPIO.output(2, False);
         GPIO.output(3, True);
         GPIO.output(4, False);
-        GPIO.output(17, False);
+        GPIO.output(14, False);
     elif (x == 2):
         GPIO.output(2, False);
         GPIO.output(3, False);
         GPIO.output(4, True);
-        GPIO.output(17, False);
+        GPIO.output(14, False);
     elif (x == 3):
         GPIO.output(2, False);
         GPIO.output(3, False);
         GPIO.output(4, False);
-        GPIO.output(17, True);
+        GPIO.output(14, True);
     else:
         GPIO.output(2, False);
         GPIO.output(3, False);
         GPIO.output(4, False);
-        GPIO.output(17, False);
+        GPIO.output(14, False);
 
