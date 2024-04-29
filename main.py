c0cfddf46f1abacbc3d63e1fa7284c43ba365b10
Anuda Weerasinghe <anuda@live.com>
2024-04-29 15:58:33 -0400

    changes width

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ class Table:
         for i in range(total_rows):
             for j in range(total_columns):
                  
-                self.e = Entry(root, width=20, fg='blue',
-                               font=('Arial',16,'bold'))
+                self.e = Entry(root, width=10, fg='blue',
+                               font=('Arial',12,'bold'))
                  
                 self.e.grid(row=i, column=j)
                 self.e.insert(END, lst[i][j])
