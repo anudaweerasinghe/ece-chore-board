f8051bf7afff402116c679d775cfff3efb43c692
Tiberius202 <tiberius202@gmail.com>
2024-04-29 22:59:39 -0400

    resizing chore chart

--- main.py
+++ main.py
@@ -133,6 +133,7 @@ set_default_next_assignments()
 # create root window
 root = Tk()
 root.title("Chore Chart")
+root.grid(row=0, column=0, sticky="wens")
 Grid.rowconfigure(root, 0, weight=1)
 Grid.columnconfigure(root, 0, weight=1)
 frame = Frame(root)
